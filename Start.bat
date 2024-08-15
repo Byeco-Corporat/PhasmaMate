16f24b06c6357e50d5903ad562ac58279039a537
byeco <borayigitelik@gmail.com>
2024-08-15 19:31:04 +0300

    bRO

--- Start.bat
+++ Start.bat
@@ -3,13 +3,7 @@
 REM Localhost sunucusunu başlatma
 echo Starting Localhost server...
 start cmd /k "cd /d %~dp0Server && node server.js"
-
-REM Electron uygulamasını başlatma
-echo Starting Electron app...
 start cmd /k "cd /d %~dp0App && npm install && npm start"
-
-REM Python scriptini başlatma
-echo Starting Python script...
 start cmd /k "cd /d %~dp0Backend && python app.py"
 
 REM Bütün işlemleri başlatıldıktan sonra komutu bekleme
